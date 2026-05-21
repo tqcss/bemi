97568ab9e69036b4cc6c36dde6b431a3c3cc3a32
tqcss <134121905+tqcss@users.noreply.github.com>
2026-05-21 11:33:22 +0800

    chore: configure chunk values

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 # Configuration module for BEMI
 
 
+SKILLS_DIR = './skills'
+CHROMA_DIR    = './data/chroma'
+
+
 #* UNSLOTH MODEL CONFIGURATION
 class unsloth_model:
-	UNSLOTH_GEMMA_4_MODEL_NAME = 'unsloth/gemma-4-E2B-it' # change to 'unsloth/gemma-4-E4B-it' if you want to use 4B model
+	GEMMA_4_MODEL_NAME = 'unsloth/gemma-4-E2B-it' # change to 'unsloth/gemma-4-E4B-it' if you want to use 4B model
 
 	GEMMA_4_PRETRAINED_PARAMS = {
-		'model_name': UNSLOTH_GEMMA_4_MODEL_NAME,
+		'model_name': GEMMA_4_MODEL_NAME,
 		'dtype': None,
 		'max_seq_length': 1024,
 		'load_in_4bit': True,
@@ -25,4 +29,9 @@ class unsloth_model:
 		'bias': 'none',
 		'random_state': 3407,
 	}
-	
+
+
+class embedding:
+	MODEL_NAME = 'embeddinggemma'
+	CHUNK_SIZE = 256
+	CHUNK_OVERLAP = 32
